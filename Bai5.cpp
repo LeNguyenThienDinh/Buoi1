731e83ebcaa93df38e6e9fe55dc6317ba813b3a6
ThienDinh <hellow12343@gmail.com>
2024-07-10 16:16:54 +0700

    completed work 5

--- Bai5.cpp
+++ Bai5.cpp
@@ -1,1 +1,26 @@
-#include
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+int main()
+{
+	int a,b,c;
+
+	printf("nhap vao so a: ");
+	scanf("%d", &a);
+	printf("nhap vao so b: ");
+	scanf("%d", &b);
+	printf("nhap vao so c: ");
+	scanf("%d", &c);
+	int thuong = c / (a - b);
+	if (a != b)
+	{
+		printf("Ket qua la: %d", thuong);
+	}
+	else
+	{
+		printf("Loi mau so phai khac 0");
+	}
+
+	return 0;
+
+}
