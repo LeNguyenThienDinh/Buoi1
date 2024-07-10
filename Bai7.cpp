012e21fb2fd9bfb50d28309bad8edc93c906c8b9
ThienDinh <hellow12343@gmail.com>
2024-07-10 16:58:17 +0700

    create function name and get ready for switch case

--- Bai7.cpp
+++ Bai7.cpp
@@ -1,5 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
+#define MAX_SIZE 100
+
+void tim_max(int *arr, int size);
+void tim_chan_lon_nhat(int *arr, int size);
+void Xoa_Phan_Tu_La_0(int *arr, int *size);
+void Them_Vao_Sau(int *arr, int *size, int x);
+int Ktra_So_CP(int n);
+int Tong_So_CP(int *arr, int size);
+void Tim_So_Cuc_Dai(int *arr, int size);
+
+int main()
+{
+
+}
 void tim_max(int *arr, int size) {
 	int *max = arr;
 	for (int i = 1; i < size; i++) {
